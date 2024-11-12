b64cfc7f31de6d19392bcbe8e39386182fc04925
ravindudinushan <ravindudinushan9966@gmail.com>
2024-11-12 15:46:37 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
