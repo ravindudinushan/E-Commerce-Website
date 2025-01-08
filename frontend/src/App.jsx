7204e63e5eadb82e9c4686d06371771ac92c329c
ravindudinushan <ravindudinushan9966@gmail.com>
2025-01-08 07:38:30 +0530

    fix issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from './component/Header'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <main>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
+    </main>
   )
 }
 
