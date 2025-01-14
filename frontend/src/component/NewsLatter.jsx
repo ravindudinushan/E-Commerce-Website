46104c35c62f770beac629260d6b6d85019ab9b0
ravindudinushan <ravindudinushan9966@gmail.com>
2025-01-14 17:29:59 +0530

    about, new letter, blog

--- frontend/src/component/NewsLatter.jsx
+++ frontend/src/component/NewsLatter.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { FaDribbble, FaFaacebookF, FaInstagram } from "react-icons/fa6";
+import { FaDribbble, FaFacebookF, FaInstagram } from "react-icons/fa6";
 
 const NewsLatter = () => {
   return (
     <section className="max-padd-container border-t-[1px] border-b-[1px] border-primary py-4">
-      <div className="flexBetween">
+      <div className="flexBetween flex-wrap gap-7">
         <div>
-          <h4>Subscribe newsletter</h4>
+          <h4 className="bold-14 uppercase tracking-wider">Subscribe newsletter</h4>
           <p>Get latest infromation on Events, Sales & Offers.</p>
         </div>
         <div>
-          <div>
-            <input type="email" placeholder="Email Address" className=""/>
-            <button>Submit</button>
-          </div>z
+          <div className="flex bg-primary">
+            <input type="email" placeholder="Email Address" className="p-4 bg-primary w-[266px] outline-none text-xs"/>
+            <button className="btn-dark !rounded-none !text-[13px] !font-bold">Submit</button>
+          </div>
         </div>
-        <div>
-            <div><FaFaacebookF /></div>
+        <div className="flex gap-x-3 pr-14">
+            <div className="h-8 w-8 rounded-full hover:bg-tertiary hover:text-white flexCenter transition-all duration-500"><FaFacebookF /></div>
         </div>
       </div>
     </section>
