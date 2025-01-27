bac72a5c2f083fb8bb58384cb615486fcc3514a0
ravindudinushan <ravindudinushan9966@gmail.com>
2025-01-27 07:41:18 +0530

    ProductDescription

--- frontend/src/component/PopularProduct.jsx
+++ frontend/src/component/PopularProduct.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Title from "./Title";
-import { products } from "../assets/data";
 import Item from "./Item";
+import { ShopContext } from "../context/ShopContext";
 
 const PopularProduct = () => {
   const [popularProducts, setPopularProducts] = useState([]);
+  const {product} = useContext(ShopContext)
 
   useEffect(() => {
     const data = products.filter((item) => item.popular);
--- frontend/src/component/RelatedProducts.jsx
+++ frontend/src/component/RelatedProducts.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Title from "./Title";
+import { products } from "../assets/data";
+import Item from "./Item";
 
-const RelatedProducts = () => {
+const RelatedProducts = ({category}) => {
+  const [popularProducts, setPopularProducts] = useState([]);
+
+  useEffect(() => {
+    const data = products.filter((item) => item.popular);
+    setPopularProducts(data.slice(0, 5));
+  },[products]);
   return (
-    <div>
-      RelatedProducts
-    </div>
-  )
-}
+    <section className="max-padd-container py-16">
+      <Title
+        title1={"Popular"}
+        title2={"Products"}
+        titleStyles={"pb-10"}
+        paraStyles={"!block"}
+      />
+      {/* CONTAINER */}
+      <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+        {popularProducts.map((product) => (
+          <div key={product._id}>
+            <Item product={product} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default RelatedProducts
+export default RelatedProducts;
