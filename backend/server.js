3416fc3fbfe8b30d0a10155fad2dc13cd914d70c
ravindudinushan <ravindudinushan9966@gmail.com>
2025-01-31 12:25:20 +0530

    config mongoDB & cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/mongodb";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // App Config
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB()
+connectDB();
+connectCloudinary;
 
 // Middleware
 app.use(express.json());
