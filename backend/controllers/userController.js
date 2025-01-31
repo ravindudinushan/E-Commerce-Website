a41a32a7b07408a8bb757567f6a9a31a437f8239
ravindudinushan <ravindudinushan9966@gmail.com>
2025-01-31 17:01:41 +0530

    routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,77 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
 // CONTROLLER FUNCTION FROM USER LOGIN
 const loginUser = async (req, res) => {
-    res.json({message: "this is login controller function for login api"})
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // CONTROLLER FUNCTION FROM USER REGISTER
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //CHAECKING IF USER ALREADY EXISTS
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    // VALIDATE PASSWORD AND CHECKING PASSWORD STRENGTH
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // HASHING USER PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // CONTROLLER FUNCTION FROM ADMIN LOGIN
 const adminLogin = async (req, res) => {};
