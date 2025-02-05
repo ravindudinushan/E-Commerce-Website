61f6f71826d8bfc34785ce1f4eeff64e5d88d7de
ravindudinushan <ravindudinushan9966@gmail.com>
2025-02-05 21:43:12 +0530

    users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,6 +74,22 @@ const registerUser = async (req, res) => {
 };
 
 // CONTROLLER FUNCTION FROM ADMIN LOGIN
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASS
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { loginUser, registerUser, adminLogin };
